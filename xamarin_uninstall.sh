2ac634461f55e05e994d414e0458f6b34f0af5d6
Amy Burns <amy.burns87@gmail.com>
2014-07-29 18:33:55 +0100

    Removing sudo and fixing smart quotes

--- xamarin_uninstall.sh
+++ xamarin_uninstall.sh
@@ -3,7 +3,7 @@
 
 # Uninstall Xamarin Studio
 
-sudo rm –rf “/Applications/Xamarin Studio.app” 
+rm –rf "/Applications/Xamarin Studio.app"
 rm –rf ~/Library/Caches/XamarinStudio-*
 rm –rf ~/Library/Logs/XamarinStudio-* 
 rm –rf ~/Library/Preferences/XamarinStudio-* 
@@ -11,34 +11,35 @@ rm –rf ~/Library/XamarinStudio-*
 
 
 # Uninstall Mono MDK
+# You will also want to make sure that there are no other dependencies on Mono
 
-sudo rm -rf /Library/Frameworks/Mono.framework
-sudo pkgutil --forget com.xamarin.mono-MDK.pkg
+rm -rf /Library/Frameworks/Mono.framework
+pkgutil --forget com.xamarin.mono-MDK.pkg
 
 
 # Uninstall Xamarin.Android
 
-sudo rm -rf /Developer/MonoDroid 
-sudo rm -rf /Library/Frameworks/Xamarin.Android.framework 
+rm -rf /Developer/MonoDroid 
+rm -rf /Library/Frameworks/Xamarin.Android.framework 
 rm -rf ~/Library/MonoAndroid 
 sudo pkgutil --forget com.xamarin.android.pkg
 
 
 # Uninstall Xamarin.iOS
 
-sudo rm -rf /Developer/MonoTouch
+rm -rf /Developer/MonoTouch
 rm -rf ~/Library/MonoTouch
-sudo pkgutil --forget com.xamarin.monotouch.pkg
-sudo pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
+pkgutil --forget com.xamarin.monotouch.pkg
+pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
 
 ## To Uninstall the Xamarn Build Host
 
-rm –rf “/Applications/Xamarin.iOS Build Host.app”
+rm –rf "/Applications/Xamarin.iOS Build Host.app"
 
 ## To uninstall unload and remove the Xamarin Build Host launchd job
 
-sudo launchctl unload /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist 
-sudo rm -f /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist
+launchctl unload /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist 
+rm -f /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist
 
 
 # Uninstall Xamarin.Mac
