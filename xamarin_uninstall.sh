04ebb30ccb11431500a47ed5bbfd99e767e125db
Amy Burns <amy.burns87@gmail.com>
2014-07-31 16:59:44 +0100

    Adding sudo back, fixing typo

--- xamarin_uninstall.sh
+++ xamarin_uninstall.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
-# This is a simple script to aid with uninstalling Xamarin from your machine.
-
+# This is a simple script to uninstall Xamarin from your machine.
+ 
 # Uninstall Xamarin Studio
-
+ 
 rm –rf "/Applications/Xamarin Studio.app"
 rm –rf ~/Library/Caches/XamarinStudio-*
-rm –rf ~/Library/Logs/XamarinStudio-* 
-rm –rf ~/Library/Preferences/XamarinStudio-* 
+rm –rf ~/Library/Logs/XamarinStudio-*
+rm –rf ~/Library/Preferences/XamarinStudio-*
 rm –rf ~/Library/XamarinStudio-*
-
-
+ 
+ 
 # Uninstall Mono MDK
 # You will also want to make sure that there are no other dependencies on Mono
-
-rm -rf /Library/Frameworks/Mono.framework
-pkgutil --forget com.xamarin.mono-MDK.pkg
-
-
+ 
+sudo rm -rf /Library/Frameworks/Mono.framework
+sudo pkgutil --forget com.xamarin.mono-MDK.pkg
+ 
+ 
 # Uninstall Xamarin.Android
-
-rm -rf /Developer/MonoDroid 
-rm -rf /Library/Frameworks/Xamarin.Android.framework 
-rm -rf ~/Library/MonoAndroid 
+ 
+sudo rm -rf /Developer/MonoDroid
+sudo rm -rf /Library/Frameworks/Xamarin.Android.framework
 sudo pkgutil --forget com.xamarin.android.pkg
-
-
+rm -rf ~/Library/MonoAndroid
+ 
+ 
 # Uninstall Xamarin.iOS
-
-rm -rf /Developer/MonoTouch
+ 
 rm -rf ~/Library/MonoTouch
-pkgutil --forget com.xamarin.monotouch.pkg
-pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
-
+sudo rm -rf /Developer/MonoTouch
+sudo pkgutil --forget com.xamarin.monotouch.pkg
+sudo pkgutil --forget com.xamarin.xamarin-ios-build-host.pkg
+ 
 ## To Uninstall the Xamarn Build Host
-
+ 
 rm –rf "/Applications/Xamarin.iOS Build Host.app"
-
-## To uninstall unload and remove the Xamarin Build Host launchd job
-
-launchctl unload /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist 
-rm -f /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist
-
-
+ 
+## To unload and remove the Xamarin Build Host launchd job
+ 
+launchctl unload /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist
+sudo rm -f /Library/LaunchAgents/com.xamarin.mtvs.buildserver.plist
+ 
+ 
 # Uninstall Xamarin.Mac
-
-rm -rf /Library/Frameworks/Xamarin.Mac.framework
-rm -rf $HOME/Library/Xamarin.Mac
+ 
+sudo rm -rf /Library/Frameworks/Xamarin.Mac.framework
+rm -rf ~/Library/Xamarin.Mac
